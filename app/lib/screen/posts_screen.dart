a90e1eced6efbc08e2eaba2254a5ee3b5647a060
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-07 12:08:36 -0800

    init post page

---
+++ app/lib/screen/posts_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class PostsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("This is a post screen"),
+    );
+  }
+}
