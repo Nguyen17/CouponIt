9aa9feb2404a3939a170bed57617f71ffe70fea2
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-07 21:48:00 -0800

    added hex code comment to colors

--- app/lib/screen/components/color.dart
+++ app/lib/screen/components/color.dart
@@ -2,4 +2,4 @@
   
 
   Color purpleColorScheme = Color.fromRGBO(184, 52, 122, 1.0);
-  Color pinkColorScheme = Color.fromRGBO(224, 41, 97, 1.0);
+  Color pinkColorScheme = Color.fromRGBO(224, 41, 97, 1.0); //#E02961
