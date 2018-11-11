a52b9a3e4e43f78ccd2d4ad7b160ebdd186889fb
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-10 16:04:53 -0800

    init coupons page

--- app/lib/screen/coupon_screen.dart
+++ app/lib/screen/coupon_screen.dart
@@ -1,75 +1,33 @@
 import 'package:flutter/material.dart';
-// import 'app_screen.dart';
 
-// /**
-//  * Importing UI Packages
-//  */
-import 'package:backdrop/backdrop.dart';
+import 'dart:async';
+import 'dart:convert';
 
-
-import 'package:firebase_database/firebase_database.dart';
-DatabaseReference database = FirebaseDatabase.instance.reference();
-
-// /**
-//  * DEBUG TEST
-//  * - create test barcode string to debug database
-//  */
-String barcodeTest = "SDFDS";
-
-
-
-// ===========================
 String couponVal;
 
-////**
-///* USAGE
-///* - Text(couponVal)
-///*   - display return barcode after scanning
-///*/
-void scannedValue(x) {
-  
+void scannedValueCoupon(x) {
   couponVal = x;
- 
-
-    // x is the hard coded coupon number value
-database.reference().child('Coupons').child(x).once().then((DataSnapshot snapshot) {
-   
-   Map<dynamic,dynamic> info = snapshot.value;
-
-   print("$info");
-   // barcodetest is the variable being printed below Text(barcodeText)
-   // acquire the value associated to Name in the database
-   barcodeTest = info['Name'];
-   print(barcodeTest);
-    });
+  
+}
+class CouponScreen extends StatefulWidget {
+  @override
+  _CouponScreenState createState() => _CouponScreenState();
 }
 
-
-
-
-
-
-
-class CouponScreen extends StatelessWidget {
+class _CouponScreenState extends State<CouponScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: BackdropScaffold(
-          title: Text("Coupons"),
-          backLayer: Center(
-            child: Text("Back Layer"),
-          ),
-          frontLayer: Center(
-            child: Text(barcodeTest),
-          ),
-          iconPosition: BackdropIconPosition.leading,
-          actions: <Widget>[
-            BackdropToggleButton(
-              icon: AnimatedIcons.list_view,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Container(
+              child: Text(couponVal),
             ),
-          ],
-        ));
-        
+          ),
+        )
+      )
+    );
   }
 }
