b250b3c07b0172345a0c60640f4f21f8d9c0af23
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-10 16:06:16 -0800

    made better comments

--- app/lib/screen/components/color.dart
+++ app/lib/screen/components/color.dart
@@ -1,5 +1,6 @@
   import 'package:flutter/material.dart';
   
 
-  Color purpleColorScheme = Color.fromRGBO(184, 52, 122, 1.0);
+  Color purpleColorScheme = Color.fromRGBO(184, 52, 122, 1.0); //#B8347A
   Color pinkColorScheme = Color.fromRGBO(224, 41, 97, 1.0); //#E02961
+  Color lightGrey = Color.fromRGBO(239,242,244, 1.0);
