b6ae9ce2af63fa36fdb20dacc6a7d743c2f9b7e2
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-10 16:05:58 -0800

    created new page for tracking prices

---
+++ app/lib/screen/price_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'components/color.dart';
+import 'package:http/http.dart' show get;
+import 'dart:async';
+import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'app_screen.dart';
+// /**
+//  * Importing UI Packages
+//  */
+// import 'package:backdrop/backdrop.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+
+DatabaseReference database = FirebaseDatabase.instance.reference();
+
+// /**
+//  * DEBUG TEST
+//  * - create test barcode string to debug database
+//  */
+String barcodeTest = "SDFDS";
+
+// ===========================
+String couponVal;
+bool _isloading = true;
+
+////**
+///* USAGE
+///* - Text(couponVal)
+///*   - display return barcode after scanning
+///*/
+void scannedValue(x) {
+  couponVal = x;
+
+//     // x is the hard coded coupon number value
+// database.reference().child('Coupons').child(x).once().then((DataSnapshot snapshot) {
+
+//    Map<dynamic,dynamic> info = snapshot.value;
+
+//    print("$info");
+//    // barcodetest is the variable being printed below Text(barcodeText)
+//    // acquire the value associated to Name in the database
+//    barcodeTest = info['Name'];
+//    print(barcodeTest);
+//     });
+}
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+ 
+  Map<dynamic, dynamic> itemModel;
+  List items;
+
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  _launchURL(urlTest) async {
+    var url = urlTest;
+    if (await canLaunch(url)) {
+      _isloading = true;
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  var url =
+      "https://api.barcodelookup.com/v2/products?barcode=${couponVal}&formatted=y&key=ca7zq4h52r9yoic9j0c10qz38upuf3";
+  @override
+  Widget build(BuildContext context) {
+    return (_isloading == true)
+        ? loadingIcon()
+        : MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SafeArea(
+                child: Scaffold(
+              body: ListView(
+                children: <Widget>[
+                  ListView.builder(
+                      itemCount: items == null ? 0 : items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        // return ListTile(
+                        //   leading: Text(items[index]['currency_symbol'] +
+                        //       items[index]['store_price']),
+                        //   title: Text(items[index]['store_name']),
+                        //   trailing: MaterialButton(
+                        //     child: Text("buy"),
+                        //     onPressed: () {
+                        //       _launchURL(items[index]['product_url']);
+                        //     },
+                        //   ),
+                        // );
+                        return Card(
+                          elevation: 2.0,
+                          child: Flex(
+                            direction: Axis.vertical,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              SizedBox(height: 20.0),
+                              Text(items[index]['store_name'],
+                                  style: TextStyle(
+                                      fontFamily: "SFProText",
+                                      fontSize: 21.0,
+                                      fontWeight: FontWeight.w600)),
+                              SizedBox(height: 20.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    items[index]['currency_symbol'] +
+                                        items[index]['store_price'],
+                                    style: TextStyle(
+                                        fontFamily: "SFProText",
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  SizedBox(width: 20.0),
+                                  Container(
+                                    width: 75.0,
+                                    padding: EdgeInsets.all(0.0),
+                                    decoration: BoxDecoration(
+                                        color: Colors.greenAccent,
+                                        borderRadius:
+                                            BorderRadius.circular(5.0)),
+                                    child: MaterialButton(
+                                      child: Text(
+                                        "buy",
+                                        style: TextStyle(
+                                            fontFamily: "SFProText",
+                                            fontSize: 10.0,
+                                            color: Colors.white),
+                                      ),
+                                      onPressed: () {
+                                        _launchURL(items[index]['product_url']);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20.0),
+                            ],
+                          ),
+                        );
+                      })
+                ],
+              ),
+            )));
+  }
+
+  Future fetchData() async {
+    var res = await get(url);
+    //debugPrint(res.body);
+    itemModel = json.decode(res.body);
+
+    setState(() {
+      items = itemModel["products"][0]["stores"];
+  
+    });
+
+    print(items);
+    print(items.length);
+    _isloading = false;
+  }
+}
+
+Widget loadingIcon() {
+  return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/world.png",
+                width: 250.0, height: 250.0),
+            SizedBox(height: 40.0),
+            Text("Getting the prices right now!",
+            style: TextStyle(
+              
+              color: Colors.black,
+              fontFamily: "SFProText",
+              fontSize: 17.0,
+              fontWeight: FontWeight.w600
+            ),),
+            SizedBox(height: 20.0),
+            SpinKitThreeBounce(
+              color: pinkColorScheme,
+              size: 21.0,
+            )
+          ],
+        ),
+      ));
+}
---
+++ app/lib/screen/price_track_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'components/color.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+
+import 'price_screen.dart';
+
+class PriceTracker extends StatefulWidget {
+  @override
+  _PriceTrackerState createState() => _PriceTrackerState();
+}
+
+class _PriceTrackerState extends State<PriceTracker> {
+  String barcode;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: lightGrey,
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: pinkColorScheme,
+              onPressed: () {
+                scan();
+              },
+              child: Icon(FontAwesomeIcons.barcode)),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          appBar: AppBar(
+            backgroundColor: pinkColorScheme,
+            leading: IconButton(
+                icon: Icon(FontAwesomeIcons.arrowLeft),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            title: Image.asset("assets/images/ic_couponitlogo_2.png"),
+            actions: <Widget>[
+              IconButton(onPressed: () {}, icon: Icon(Icons.search))
+            ],
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("assets/images/loading.png",
+                    width: 300.0, height: 300.0),
+                SizedBox(height: 20.0),
+                AutoSizeText(
+                  "Search for a price.",
+                  style: TextStyle(
+                      fontFamily: "SFProText",
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 20.0),
+                AutoSizeText("Tap on button below.",
+                    style: TextStyle(
+                        fontFamily: "SFProText",
+                        fontSize: 11.0,
+                        color: Colors.black45)),
+                SizedBox(height: 10.0),
+                Icon(
+                  Icons.arrow_downward,
+                  color: Colors.black45,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      scannedValue(barcode);
+      setState(() => this.barcode = barcode);
+      Navigator.pushNamed(context, '/price.');
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
+  }
+}
