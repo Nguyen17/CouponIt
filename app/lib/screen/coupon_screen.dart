83c190118ab23b4265ab26f04f3a9f5e147906b1
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-11 21:21:39 -0800

    coupons now renders when an item is scanned

--- app/lib/screen/coupon_screen.dart
+++ app/lib/screen/coupon_screen.dart
@@ -1,33 +1,117 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'components/data/coupons.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 
 String couponVal;
 
 void scannedValueCoupon(x) {
   couponVal = x;
-  
 }
+
 class CouponScreen extends StatefulWidget {
   @override
   _CouponScreenState createState() => _CouponScreenState();
 }
 
 class _CouponScreenState extends State<CouponScreen> {
+  Map<dynamic, dynamic> productModel;
+  List coupons;
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Container(
-              child: Text(couponVal),
-            ),
-          ),
-        )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          
+        
+ 
+            // Container(
+            //     height: 400.0,
+            //     decoration: BoxDecoration(
+            //         image: DecorationImage(
+            //       fit: BoxFit.cover,
+            //       image: NetworkImage(
+            //           productModel["073854008089"][0]["itemImgUrl"]),
+            //     ))),
+            //     SizedBox(height:20.0),
+            body:ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: productModel["073854008089"][0]["coupons"].length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                  
+                    contentPadding: EdgeInsets.all(20.0),
+                    leading: AutoSizeText(
+                      productModel["073854008089"][0]["coupons"][index]
+                          ["couponType"],
+                      softWrap: true,
+                      maxLines: 3,
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          fontFamily: "SFProText",
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    title: AutoSizeText(
+                        productModel["073854008089"][0]["coupons"][index]
+                            ["couponTitle"],
+                        maxLines: 3,
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontFamily: "SFProText",
+                          fontSize: 11.0,
+                        )),
+                    trailing: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(10.0)
+                      ),
+                      child: MaterialButton(
+                        onPressed: (){},
+                        child: AutoSizeText(
+                      productModel["073854008089"][0]["coupons"][index]
+                          ["couponCode"],
+                      maxLines: 3,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "SFProText",
+                          fontSize: 10.0,
+                        )
+
+                    ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )
+          ,
+        ));
+  }
+
+  /** 
+   * Fetch Coupons data
+   */
+
+  retrieveData() {
+    productModel = products;
+
+    print(productModel);
+    print(productModel["073854008089"][0]["coupons"][0]["couponType"]);
+    print(productModel["073854008089"][0]["coupons"][0]["couponTitle"]);
+    print(productModel["073854008089"][0]["coupons"][0]["couponCode"]);
   }
 }
