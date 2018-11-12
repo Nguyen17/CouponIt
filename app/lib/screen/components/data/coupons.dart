5d2bf623966ff699a08533c931323d963a9e18a7
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-11 21:22:03 -0800

    init data

---
+++ app/lib/screen/components/data/coupons.dart
@@ -0,0 +1,100 @@
+final products = {
+  "073854008089": [
+    {
+      "itemName": "Bicycle Playing Cards",
+      "itemImgUrl": "https://bit.ly/2JV3sIV",
+      "coupons": [
+        {
+          "couponTitle": "10% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AFBY810"
+        },
+        {
+          "couponTitle": "25% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "25% OFF",
+          "couponCode": "ZGe70"
+        },
+        {
+          "couponTitle": "Buy one and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "BUE395"
+        },
+            {
+          "couponTitle": "10% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AFBY810"
+        },
+        {
+          "couponTitle": "25% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "25% OFF",
+          "couponCode": "ZGe70"
+        },
+        {
+          "couponTitle": "Buy one and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "BUE395"
+        },
+            {
+          "couponTitle": "10% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AFBY810"
+        },
+        {
+          "couponTitle": "25% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "25% OFF",
+          "couponCode": "ZGe70"
+        },
+        {
+          "couponTitle": "Buy one and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "BUE395"
+        },
+            {
+          "couponTitle": "10% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AFBY810"
+        },
+        {
+          "couponTitle": "25% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "25% OFF",
+          "couponCode": "ZGe70"
+        },
+        {
+          "couponTitle": "Buy one and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "BUE395"
+        },
+            {
+          "couponTitle": "10% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AFBY810"
+        },
+        {
+          "couponTitle": "25% off Bicycle Classic Playing Cards ",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "25% OFF",
+          "couponCode": "ZGe70"
+        },
+        {
+          "couponTitle": "Buy one and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "BUE395"
+        }
+      ]
+    },
+  ]
+};
