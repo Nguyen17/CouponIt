e68bc54fcd9418817e206ce7d3421520e2b72ed6
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-20 23:06:26 -0800

    redesigned website

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+
+new  AOS.init();
+
+
+$(document).scroll(function(event){
+    
+    var scrollPos = $(window).scrollTop();
+    console.log(scrollPos);
+
+        if(scrollPos >= 2520){
+            $(".navbar h3").css({"color":"#ac1886"});
+            $(".navbar a").css({"color":"#ac1886"});
+            $("fa fa-navicon").css({"color":"#ac1886"});
+        }
+        if(scrollPos < 2520){
+            $(".navbar h3").css({"color":"white"});
+            $(".navbar a").css({"color":"white"});
+            $("fa fa-navicon").css({"color":"white"});
+        }
+    // if(scrollPos >= 423){
+    //     $(".fixed-navbar").css({"height": "70px"});
+    //     $(".fixed-navbar img").css({"height": "70px"});
+    //     $(".fixed-navbar  #menu-icon").css({"height": "40px"});
+    // }
+    // else{
+    //     $(".fixed-navbar").css({"height": "0px"});
+    //     $(".fixed-navbar img").css({"height": "0px"});
+    //     $(".fixed-navbar #menu-icon").css({"height": "0px"});
+    // };
+
+
+});
