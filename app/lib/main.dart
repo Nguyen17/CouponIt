dcf333d0eae53c63076d972004b0e3eca9b84d80
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-24 10:41:56 -0800

    added onesignal

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal/onesignal.dart';
+import 'dart:async';
+import 'dart:convert';
 // import 'package:firebase_database/firebase_database.dart';
 
 // /**
@@ -20,34 +23,71 @@ import 'screen/price_track_screen.dart';
 import 'screen/coupon_screen.dart';
 import 'screen/splash_screen.dart';
 
+// DEBUG
+import './models/notification_model.dart';
 
-void main(){
+void main() {
   runApp(App());
 }
 
 // /**
-//  * App 
+//  * App
 //  * * This is the main file
 //  * * - contains routes that store the location of pages
 //  */
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
 
-class App extends StatelessWidget {
+class _AppState extends State<App> {
+
+  
+  String _debugMessage = "";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initAppState();
+  }
+
+  Future initAppState() async {
+
+       OneSignal.shared.setNotificationReceivedHandler((notification) {
+        
+        print("incoming message");
+      this.setState(() {
+        _debugMessage =
+            "${notification.jsonRepresentation()}";
+      });
+    });
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      print("received again");
+
+    });
+
+    await OneSignal.shared.init("4551c22d-d57e-449a-b60e-a9f435b65f3e");
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Coupon It",
       initialRoute: '/',
-      routes:{
+      routes: {
         '/': (context) => WelcomeScreen(),
-        '/login':(context) => LoginScreen(),
+        '/login': (context) => LoginScreen(),
         '/sign_up': (context) => SignUpScreen(),
-        '/sign_up_email':(context) => SignUpEmail(),
-        '/sign_up_password':  (context)=>SignUpPassword(),
-        '/profile': (context)=>ProfileScreen(),
+        '/sign_up_email': (context) => SignUpEmail(),
+        '/sign_up_password': (context) => SignUpPassword(),
+        '/profile': (context) => ProfileScreen(),
         '/account_settings': (context) => AccountSettingsScreen(),
-        '/menu':(context) => MenuScreen(),
-        '/price':(context) => PriceScreen(),
+        '/menu': (context) => MenuScreen(),
+        '/price': (context) => PriceScreen(),
         '/home': (context) => AppScreen(),
         '/coupon': (context) => CouponScreen(),
         '/price_track': (context) => PriceTracker()
