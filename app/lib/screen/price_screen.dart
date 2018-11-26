b4f3e96557f4c4be83577a01167add2223ea9878
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-25 19:29:29 -0800

    fixing price screen

--- app/lib/screen/price_screen.dart
+++ app/lib/screen/price_screen.dart
@@ -41,6 +41,7 @@ void scannedValue(x) {
 //    print(barcodeTest);
 //     });
 }
+  String testBarcode= "073854008089";
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -54,7 +55,7 @@ class _PriceScreenState extends State<PriceScreen> {
 
   void initState() {
     super.initState();
-    // fetchData();
+    fetchData();
   
   }
 
@@ -68,87 +69,97 @@ class _PriceScreenState extends State<PriceScreen> {
     }
   }
 
-  var url =
-      "https://api.barcodelookup.com/v2/products?barcode=${priceVal}&formatted=y&key=ca7zq4h52r9yoic9j0c10qz38upuf3";
+
+  var apiToken = "auvc3z69w04brkitdxg03bx5r1lhem";
+  var url ="https://api.barcodelookup.com/v2/products?barcode=$priceVal&formatted=y&key=auvc3z69w04brkitdxg03bx5r1lhem";
   @override
   Widget build(BuildContext context) {
+
+    // return MaterialApp(
+    //   home:Scaffold(
+    //     body: Center(
+    //       child: Container(
+    //         child: Text(priceVal)
+    //       ),
+    //     )
+    //   )
+    // );
     return (_isloading == true)
-        ? loadingIcon() : MaterialApp(home: Scaffold(body: Center(child: Text(priceVal))));
-        // : MaterialApp(
-        //     debugShowCheckedModeBanner: false,
-        //     home: SafeArea(
-        //         child: Scaffold(
-        //       body: ListView(
-        //         children: <Widget>[
-        //           ListView.builder(
-        //               itemCount: items == null ? 0 : items.length,
-        //               itemBuilder: (BuildContext context, int index) {
-        //                 // return ListTile(
-        //                 //   leading: Text(items[index]['currency_symbol'] +
-        //                 //       items[index]['store_price']),
-        //                 //   title: Text(items[index]['store_name']),
-        //                 //   trailing: MaterialButton(
-        //                 //     child: Text("buy"),
-        //                 //     onPressed: () {
-        //                 //       _launchURL(items[index]['product_url']);
-        //                 //     },
-        //                 //   ),
-        //                 // );
-        //                 return Card(
-        //                   elevation: 2.0,
-        //                   child: Flex(
-        //                     direction: Axis.vertical,
-        //                     mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //                     children: <Widget>[
-        //                       SizedBox(height: 20.0),
-        //                       Text(items[index]['store_name'],
-        //                           style: TextStyle(
-        //                               fontFamily: "SFProText",
-        //                               fontSize: 21.0,
-        //                               fontWeight: FontWeight.w600)),
-        //                       SizedBox(height: 20.0),
-        //                       Row(
-        //                         mainAxisAlignment: MainAxisAlignment.center,
-        //                         children: <Widget>[
-        //                           Text(
-        //                             items[index]['currency_symbol'] +
-        //                                 items[index]['store_price'],
-        //                             style: TextStyle(
-        //                                 fontFamily: "SFProText",
-        //                                 fontSize: 14.0,
-        //                                 fontWeight: FontWeight.w600),
-        //                           ),
-        //                           SizedBox(width: 20.0),
-        //                           Container(
-        //                             width: 75.0,
-        //                             padding: EdgeInsets.all(0.0),
-        //                             decoration: BoxDecoration(
-        //                                 color: Colors.greenAccent,
-        //                                 borderRadius:
-        //                                     BorderRadius.circular(5.0)),
-        //                             child: MaterialButton(
-        //                               child: Text(
-        //                                 "buy",
-        //                                 style: TextStyle(
-        //                                     fontFamily: "SFProText",
-        //                                     fontSize: 10.0,
-        //                                     color: Colors.white),
-        //                               ),
-        //                               onPressed: () {
-        //                                 _launchURL(items[index]['product_url']);
-        //                               },
-        //                             ),
-        //                           ),
-        //                         ],
-        //                       ),
-        //                       SizedBox(height: 20.0),
-        //                     ],
-        //                   ),
-        //                 );
-        //               })
-        //         ],
-        //       ),
-        //     )));
+        // ? loadingIcon() : MaterialApp(home: Scaffold(body: Center(child: Text(priceVal))));
+      ? loadingIcon() : MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SafeArea(
+                child: Scaffold(
+              body:
+                  ListView.builder(
+                      itemCount: items == null ? 0 : items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          leading: Text(items[index]['currency_symbol'] +
+                              items[index]['store_price']),
+                          title: Text(items[index]['store_name']),
+                          trailing: MaterialButton(
+                            child: Text("buy"),
+                            onPressed: () {
+                              _launchURL(items[index]['product_url']);
+                            },
+                          ),
+                        );
+                        // return Card(
+                        //   elevation: 2.0,
+                        //   child: Flex(
+                        //     direction: Axis.vertical,
+                        //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        //     children: <Widget>[
+                        //       SizedBox(height: 20.0),
+                        //       Text(items[index]['store_name'],
+                        //           style: TextStyle(
+                        //               fontFamily: "SFProText",
+                        //               fontSize: 21.0,
+                        //               fontWeight: FontWeight.w600)),
+                        //       SizedBox(height: 20.0),
+                        //       Row(
+                        //         mainAxisAlignment: MainAxisAlignment.center,
+                        //         children: <Widget>[
+                        //           Text(
+                        //             items[index]['currency_symbol'] +
+                        //                 items[index]['store_price'],
+                        //             style: TextStyle(
+                        //                 fontFamily: "SFProText",
+                        //                 fontSize: 14.0,
+                        //                 fontWeight: FontWeight.w600),
+                        //           ),
+                        //           SizedBox(width: 20.0),
+                        //           Container(
+                        //             width: 75.0,
+                        //             padding: EdgeInsets.all(0.0),
+                        //             decoration: BoxDecoration(
+                        //                 color: Colors.greenAccent,
+                        //                 borderRadius:
+                        //                     BorderRadius.circular(5.0)),
+                        //             child: MaterialButton(
+                        //               child: Text(
+                        //                 "buy",
+                        //                 style: TextStyle(
+                        //                     fontFamily: "SFProText",
+                        //                     fontSize: 10.0,
+                        //                     color: Colors.white),
+                        //               ),
+                        //               onPressed: () {
+                        //                 _launchURL(items[index]['product_url']);
+                        //               },
+                        //             ),
+                        //           ),
+                        //         ],
+                        //       ),
+                        //       SizedBox(height: 20.0),
+                        //     ],
+                        //   ),
+                        // );
+                      })
+            
+         
+            )));
   }
 
   Future fetchData() async {
