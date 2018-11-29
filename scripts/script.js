8940cf56338b56c0a67c38ccc58a0f29288d7977
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-29 12:45:00 -0800

    modified scrolling y position

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,9 @@ new  AOS.init();
 $(document).scroll(function(event){
     
     var scrollPos = $(window).scrollTop();
-    console.log(scrollPos);
+    // console.log(scrollPos);
 
-        if(scrollPos >= 2520){
+        if(scrollPos >= 2318){
             $(".navbar h3").css({"color":"#ac1886"});
             $(".navbar a").css({"color":"#ac1886"});
             $("fa fa-navicon").css({"color":"#ac1886"});
