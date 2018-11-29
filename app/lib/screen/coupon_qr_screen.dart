3fe629cbafb6da7d93c323e04be2dc41a611be99
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-11-29 12:37:45 -0800

    updated UI

--- app/lib/screen/coupon_qr_screen.dart
+++ app/lib/screen/coupon_qr_screen.dart
@@ -17,58 +17,98 @@ class _CouponQRState extends State<CouponQR> {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Color.fromRGBO(255, 31 ,105, 1.0),
+          backgroundColor: Color.fromRGBO(255, 31, 105, 1.0),
           body: Center(
             child: Container(
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Text("Scan Code.",
-                    style: TextStyle(
-                      fontFamily: "Cookie",
-                      fontSize: 44.0,
-                      color: Color.fromRGBO(255,255,255, 1.0)
-                    ),),
-                    SizedBox(
-                      height: 80.0,
+                    Text(
+                      "Scan Code.",
+                      style: TextStyle(
+                          fontFamily: "Kotori",
+                          fontSize: 34.0,
+                          color: Color.fromRGBO(255, 255, 255, 1.0)
+                          
+                          ),
                     ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                   
                     new QrImage(
                       data: couponCode,
-                      size: 250.0,
+                      size: 280.0,
                       foregroundColor: Colors.white,
                     ),
                     SizedBox(height: 40.0),
-                    Text(couponCode,
+                     Text("or type",
                     style: TextStyle(
+                      color: Colors.white,
                       fontFamily: "SFProText",
-                      fontSize: 21.0,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.greenAccent
+                      fontSize: 14.0
                     ),),
-                    SizedBox(height:10.0),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   Text("share to friends: ",
-                   style: TextStyle(
-                     fontFamily: "SFProText",
-                     fontSize: 11.0,
-                     color: Colors.white
-                   ),),
-                        IconButton(
-                      onPressed: (){
-                        Share.share(
-                          "Your friend shared a coupon!: \n" + couponImageLink 
-                          + "\n" + couponDescription + "coupon code: " + couponCode 
-                        );
-                      },
-                      icon: Icon(CommunityMaterialIcons.telegram,
-                      color: Colors.white),
+                    SizedBox(height: 30.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                            color:Colors.black38,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            couponCode,
+                            style: TextStyle(
+                                fontFamily: "SFProText",
+                                fontSize: 21.0,
+                                fontWeight: FontWeight.w700,
+                                // color: Color.fromRGBO(159,253,198, 1.0)),
+                                color: Colors.white
+                                
+                                )
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 40.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          "share with friends: ",
+                          style: TextStyle(
+                              fontFamily: "SFProText",
+                              fontSize: 15.0,
+                              color: Colors.white
+                              // color:Colors.black54
+                              ),
+                        ),
+                        Container(
+                          width: 44.0,
+                          height: 44.0,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: IconButton(
+                            onPressed: () {
+                              Share.share("Your friend shared a coupon!: \n" +
+                                  couponImageLink +
+                                  "\n" +
+                                  couponDescription +
+                                  "\n" +
+                                  "coupon code: " +
+                                  couponCode);
+                            },
+                            icon: Icon(CommunityMaterialIcons.telegram,
+                                color: Colors.white),
+                          ),
+                        )
+                      ],
                     )
-
-                 ],
-               )
                   ]),
             ),
           ),
