679e53951e3117beee0972d16e978e1ba874de2e
JeffinVegas <razonj1@unlv.nevada.edu>
2018-11-30 12:00:52 -0800

    more coupons

--- app/lib/screen/components/data/coupons.dart
+++ app/lib/screen/components/data/coupons.dart
@@ -104,10 +104,46 @@ final products = {
           "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
       "coupons": [
         {
-          "couponTitle": "20% of drinking water",
+          "couponTitle": "20% off drinking water",
           "couponSrcUrl": "https://www.bicyclecards.com/",
           "couponType": "20% OFF",
-          "couponCode": "YUI4708"
+          "couponCode": "RcD834"
+        },
+        {
+          "couponTitle": "Buy two 24 packs and Get one Half Off",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "50% OFF",
+          "couponCode": "ITg788"
+        }
+      ]
+    }
+  ],
+  "037000917816": [
+    {
+      "itemName": "Tide Pods",
+      "itemImgUrl":
+          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
+      "coupons": [
+        {
+          "couponTitle": "10% off Tide Pod purchases",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "10% OFF",
+          "couponCode": "AVZ649"
+        }
+      ]
+    }
+  ],
+  "080878171255": [
+    {
+      "itemName": "Pantene Pro-V Daily Moisture Renewal Conditioner",
+      "itemImgUrl":
+          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
+      "coupons": [
+        {
+          "couponTitle": "Special 30% off Pantene Pro-V",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "30% OFF",
+          "couponCode": "POJ9350"
         }
       ]
     }
