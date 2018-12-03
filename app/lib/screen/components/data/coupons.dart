cbec775c68d7c09346e114dad6c7c7dfe7eceb17
JeffinVegas <razonj1@unlv.nevada.edu>
2018-12-03 15:19:57 -0800

    more coupons

--- app/lib/screen/components/data/coupons.dart
+++ app/lib/screen/components/data/coupons.dart
@@ -1,4 +1,5 @@
 final products = {
+  //PRODUCT 1
   "073854008089": [
     {
       "itemName": "Bicycle Playing Cards",
@@ -97,6 +98,8 @@ final products = {
       ]
     },
   ],
+
+  //PRODUCT 2
   "811371511169": [
     {
       "itemName": "Casa Cardenas Purified Drinking Water",
@@ -110,7 +113,7 @@ final products = {
           "couponCode": "RcD834"
         },
         {
-          "couponTitle": "Buy two 24 packs and Get one Half Off",
+          "couponTitle": "Buy two 32 packs and Get one Half Off",
           "couponSrcUrl": "https://www.bicyclecards.com/",
           "couponType": "50% OFF",
           "couponCode": "ITg788"
@@ -118,32 +121,105 @@ final products = {
       ]
     }
   ],
+
+  //PRODUCT 3
+  "492714001068": [
+    {
+      "itemName": "Aquafina Purified Drinking Water",
+      "itemImgUrl":
+          "https://www.aquafina.com/images/our-products/product_20oz.png",
+      "coupons": [
+        {
+          "couponTitle": "10% off Aquafina",
+          "couponSrcUrl": "http://www.aquafina.com/en-US/",
+          "couponType": "10% OFF",
+          "couponCode": "TTF347"
+        },
+        {
+          "couponTitle": "Buy 4 packs for the Price of Three",
+          "couponSrcUrl": "http://www.aquafina.com/en-US/",
+          "couponType": "25% OFF",
+          "couponCode": "ZNU39134"
+        }
+      ]
+    }
+  ],
+
+  //PRODUCT 4 
   "037000917816": [
     {
       "itemName": "Tide Pods",
       "itemImgUrl":
-          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
+          "https://azcdn.galileo.pgsitecore.com/en-us/-/media/Tide/Images/Products/Listing/Pods/new/80310481_91527484_List.png?h=300&la=en-US&w=300&v=1-201801191033",
       "coupons": [
         {
           "couponTitle": "10% off Tide Pod purchases",
-          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponSrcUrl": "https://tide.com/en-us/shop/type/laundry-pods",
           "couponType": "10% OFF",
           "couponCode": "AVZ649"
         }
       ]
     }
   ],
+
+  //PRODUCT 5
   "080878171255": [
     {
       "itemName": "Pantene Pro-V Daily Moisture Renewal Conditioner",
       "itemImgUrl":
-          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
+          "https://images-na.ssl-images-amazon.com/images/I/71e4yRE8HTL._SL1500_.jpg",
       "coupons": [
         {
           "couponTitle": "Special 30% off Pantene Pro-V",
-          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponSrcUrl": "https://pantene.com/en-us/product/daily-moisture-renewal-conditioner",
           "couponType": "30% OFF",
           "couponCode": "POJ9350"
+        },
+        {
+          "couponTitle": "Both Pro-V Shampoo & Conditioner for Price of ONe",
+          "couponSrcUrl": "https://pantene.com/en-us/product/daily-moisture-renewal-conditioner",
+          "couponType": "100% OFF",
+          "couponCode": "GJB4899"
+        }
+      ]
+    }
+  ],
+
+  //PRODUCT 6
+  "859896004018": [
+    {
+      "itemName": "Suavecito Firme (Strong) Hold Pomade",
+      "itemImgUrl":
+          "https://cdn.shopify.com/s/files/1/0274/1389/products/suavecito-firme-hold-pomade-4oz-side_large.png?v=1535585758",
+      "coupons": [
+        {
+          "couponTitle": "10% off Suavecito Gel",
+          "couponSrcUrl": "https://www.suavecito.com/",
+          "couponType": "10% OFF",
+          "couponCode": "BTJ589"
+        },
+        {
+          "couponTitle": "Christmas Special: 15% off every Suavecito purchase",
+          "couponSrcUrl": "https://www.suavecito.com/",
+          "couponType": "15% OFF",
+          "couponCode": "YRQ119"
+        }
+      ]
+    }
+  ],
+
+  //PRODUCT 7
+  "8802035116785": [
+    {
+      "itemName": "Rilakkuma Pencil Pouch",
+      "itemImgUrl":
+          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/03/15/ok-hand.jpg?w968h681",
+      "coupons": [
+        {
+          "couponTitle": "Buy 1 Get 1 FREE",
+          "couponSrcUrl": "https://www.bicyclecards.com/",
+          "couponType": "100% OFF",
+          "couponCode": "GBO054"
         }
       ]
     }
