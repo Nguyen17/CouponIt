f37bee69a90f9826542662c032dc91eb784d1ed7
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-12-03 23:03:17 -0800

    added new color

--- app/lib/screen/components/color.dart
+++ app/lib/screen/components/color.dart
@@ -3,4 +3,5 @@
 
   Color purpleColorScheme = Color.fromRGBO(184, 52, 122, 1.0); //#B8347A
   Color pinkColorScheme = Color.fromRGBO(224, 41, 97, 1.0); //#E02961
+  Color pinkMono1 =Color.fromRGBO(227,63,113, 1.0); // #E33F71
   Color lightGrey = Color.fromRGBO(239,242,244, 1.0);
